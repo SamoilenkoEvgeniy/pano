7fd06d78a6ad94c1dd0d5f58faea04ed14c7e19d
samoilenkoevgeniy <samoilenkoevgeniy@gmail.com>
2021-02-10 21:59:10 +0100

    init

---
+++ index.js
@@ -0,0 +1,3 @@
+const panorama = new PANOLENS.ImagePanorama( './16.jpg' );
+const viewer = new PANOLENS.Viewer();
+viewer.add( panorama );
---
+++ public/index.js
@@ -0,0 +1,3 @@
+const panorama = new PANOLENS.ImagePanorama( './static/16.jpg' );
+const viewer = new PANOLENS.Viewer();
+viewer.add( panorama );

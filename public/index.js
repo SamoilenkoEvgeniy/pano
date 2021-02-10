6006b33ecab3c106938878bf771229ac96cf61a0
samoilenkoevgeniy <samoilenkoevgeniy@gmail.com>
2021-02-10 22:18:10 +0100

    test

--- public/index.js
+++ public/index.js
@@ -1,3 +1,3 @@
-const panorama = new PANOLENS.ImagePanorama( './static/16.jpg' );
+const panorama = new PANOLENS.ImagePanorama( './static/19.jpg' );
 const viewer = new PANOLENS.Viewer();
 viewer.add( panorama );

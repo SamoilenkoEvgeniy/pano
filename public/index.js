2fa79c2842045dd3bfa27e3bf36d55abfb61c129
samoilenkoevgeniy <samoilenkoevgeniy@gmail.com>
2021-03-20 11:58:50 +0100

    test2

--- public/index.js
+++ public/index.js
@@ -1,4 +1,10 @@
 console.log(window.location.href);
+
+const url_string = window.location.href;
+const url = new URL(url_string);
+const paramValue = url.searchParams.get("img");
+alert(paramValue);
+
 const panorama = new PANOLENS.ImagePanorama( './static/19.jpg' );
 const viewer = new PANOLENS.Viewer();
 viewer.add( panorama );
